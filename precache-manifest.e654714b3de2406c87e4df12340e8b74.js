c6ee37d520065f52b0ac3b8861ddce53a8aa4a9b
Santiago Montoya <xantiagoma@gmail.com>
2020-11-19 02:24:41 -0500

    Updates

---
+++ precache-manifest.e654714b3de2406c87e4df12340e8b74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9e846c28ada5b65b4e9491e69b33c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f69ff40e1713e18db989",
+    "url": "/static/css/2.e9cb761b.chunk.css"
+  },
+  {
+    "revision": "edb072c0a84161828aae",
+    "url": "/static/css/main.b82f5c1e.chunk.css"
+  },
+  {
+    "revision": "f69ff40e1713e18db989",
+    "url": "/static/js/2.a696f88e.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/static/js/2.a696f88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb072c0a84161828aae",
+    "url": "/static/js/main.81f8ef5c.chunk.js"
+  },
+  {
+    "revision": "b521836d2555bae9f798",
+    "url": "/static/js/runtime-main.145304cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abd4bf8cebead7ab442894671b2a8979.js"
+  "/precache-manifest.e654714b3de2406c87e4df12340e8b74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
